439f7b4ec296a10c7dcc2e3050fde68e661a7c26
RizalAlfadlil <rizalalfadlil@gmail.com>
2023-10-08 10:07:09 +0700

    edit sidebar

--- src/components/sidebar/Navigation.js
+++ src/components/sidebar/Navigation.js
@@ -6,13 +6,14 @@ import 'font-awesome/css/font-awesome.min.css';
 export const Navigation = () =>{
     return(
         <div className='box box-dark navigation'>
-            <b className='box-title-dark'>Navigasi<hr/></b>
-            <ul type='none'>
-                <li><a href='/'><i className="fa fa-home m-2"/>Dashboard</a></li>
-                <li><a href='/siswa'><i className="fa fa-user m-2"/>Data Siswa</a></li>
-                <li><a href='/buku'><i className="fa fa-book m-2"/>Data Buku</a></li>
-                <li><a href='/admin'><i className="fa fa-cog m-2"/>Data Admin</a></li>
-            </ul>
+            <b className='box-title-dark'><i className='fa fa-bars m-2'/>Navigasi<hr/></b>
+            <div className='flex-wrap'>
+            <a href='/'><i className="fa fa-home m-2 xl"/><br/>Dashboard</a>
+            <a href='/siswa'><i className="fa fa-user m-2"/><br/>Data Siswa</a>
+            <a href='/buku'><i className="fa fa-book m-2"/><br/>Data Buku</a>
+            <a href='/admin'><i className="fa fa-shield m-2"/><br/>Data Admin</a>
+            <a href='/'><i className="fa fa-cog m-2"/><br/>Pengaturan</a>
+            </div>
         </div>
     )
 }
--- src/components/sidebar/accountinfo.js
+++ src/components/sidebar/accountinfo.js
@@ -1,10 +1,11 @@
 import 'bootstrap/dist/css/bootstrap.min.css'; // Import CSS Bootstrap
 import '../../css/style.css';
+import 'font-awesome/css/font-awesome.min.css';
 
 export const AccountInfo = () =>{
     return(
         <div className='box accountinfo'>
-            <b className='box-title'>Akun<hr/></b>
+            <b className='box-title'><i className='fa fa-user m-2'/>Akun<hr/></b>
             <div className='box-container'>
                 <div>
                     <div className='acImg m-2'/>
